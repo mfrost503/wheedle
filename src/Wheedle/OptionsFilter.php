3d9882bd906e041f84e0252b02f1046330f72f9e
Matt Frost <mfrost.design@gmail.com>
2015-01-07 23:46:42 -0500

    Guess I was an idiot here too

--- src/Wheedle/OptionsFilter.php
+++ src/Wheedle/OptionsFilter.php
@@ -2,20 +2,20 @@
 namespace Wheedle;
 
 /**
+ * A trait to handle the filtering of query string params
+ *
  * @author Matt Frost <mfrost.design@gmail.com
  * @license MIT http://opensource.org/licenses/MIT
  * @package Wheedle
- *
- * A trait to handle the filtering of query string params
  */
 trait OptionsFilter
 {
     /**
+     * Method to filter out any unavailable parameters
+     *
      * @param Array $availableOptions An array of available parameters
      * @param Array $options An array of the provided parameters
      * @return Arrary
-     *
-     * Method to filter out any unavailable parameters
      */
     public function filterOptions($availableOptions, $options)
     {
