4e185183a763fcc7461bcf75556ed7e32c5a1901
Matt Frost <mfrost.design@gmail.com>
2015-01-26 19:00:51 -0500

    Adding a new exception folder...with some carefully crafted exceptions

---
+++ src/Wheedle/Exceptions/RateLimitExceededException.php
@@ -0,0 +1,13 @@
+<?php
+namespace Wheedle\Exceptions;
+
+/**
+ * @author Matt Frost <mfrost.design@gmail.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ * @package Wheedle
+ * @subpackage Exceptions
+ */
+class RateLimitExceededException extends \Exception
+{
+    
+}
